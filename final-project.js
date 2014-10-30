296edccc03128ef894099df999ad6791200a66cb
Jacob Oakes <jacoboakes92@gmail.com>
2014-10-30 12:36:47 -0400

    able to draw spheres

---
+++ final-project.js
@@ -0,0 +1,125 @@
+var canvas;
+var texture; 
+var vertices = 0;
+var textureCoordData = [];
+var vertexData = [];
+
+window.onload = function init() {
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    initializeTexture(program);
+
+    createSphere(.5, .5, .5, .5);
+    createSphere(0, 0, 0, .25);
+
+    initializeBuffers(program);
+    
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+       
+    render();
+}
+
+var initializeTexture = function(program){
+    texture = gl.createTexture();
+    texture.image = new Image();
+    texture.image.onload = function(){
+        gl.bindTexture (gl.TEXTURE_2D, texture);
+        gl.texImage2D (gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, texture.image);
+        gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.uniform1i(gl.getUniformLocation(program, "texMap"), 0);
+    }
+    texture.image.src = "earth_texture.gif";
+}
+
+var createSphere = function(centerX, centerY, centerZ, radius){
+    var latitudeBands = 30;
+    var longitudeBands = 30;
+    var vertexPositionData = [];
+    
+    for (var latNumber=0; latNumber <= latitudeBands; latNumber++) {
+        var theta = latNumber * Math.PI / latitudeBands;
+        var sinTheta = Math.sin(theta);
+        var cosTheta = Math.cos(theta);
+        var temp = [];
+        for (var longNumber=0; longNumber <= longitudeBands; longNumber++) {
+            var phi = longNumber * 2 * Math.PI / longitudeBands;
+            var sinPhi = Math.sin(phi);
+            var cosPhi = Math.cos(phi);
+
+            var x = cosPhi * sinTheta;
+            var y = cosTheta;
+            var z = sinPhi * sinTheta;
+            temp.push(vec4(radius * x + centerX, radius * y + centerY, radius * z + centerZ, 1));
+        }
+        vertexPositionData.push(temp);
+    }
+
+    for (var latNumber=0; latNumber <= latitudeBands; latNumber++) {
+        for (var longNumber=0; longNumber <= longitudeBands; longNumber++) {
+            nextLat = latNumber == latitudeBands ? 0 : latNumber + 1;
+            nextLong = longNumber == latitudeBands ? 0 : longNumber + 1;
+            vertexData.push(
+                vertexPositionData[latNumber][longNumber],
+                vertexPositionData[nextLat][longNumber],
+                vertexPositionData[latNumber][nextLong]
+            );
+            vertexData.push(
+                vertexPositionData[latNumber][nextLong],
+                vertexPositionData[nextLat][longNumber],
+                vertexPositionData[nextLat][nextLong]
+            );
+            vertices+=6;
+        }
+    }
+}
+
+var initializeBuffers = function(program){
+    // var cBuffer = gl.createBuffer();
+    // gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    // gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
+    
+    // var vColor = gl.getAttribLocation( program, "vColor" );
+    // gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    // gl.enableVertexAttribArray( vColor);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexData), gl.STATIC_DRAW );
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+    
+    // var tBuffer = gl.createBuffer();
+    // gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, flatten(textureCoordData), gl.STATIC_DRAW);
+    
+    // var vTexCoord = gl.getAttribLocation (program, "vTexCoord");
+    // gl.vertexAttribPointer (vTexCoord, 2, gl.FLOAT, false, 0, 0);
+    // gl.enableVertexAttribArray(vTexCoord);
+ 
+}
+
+
+var render = function() {
+        gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);        
+            
+        gl.drawArrays( gl.TRIANGLES, 0, vertices);
+        requestAnimFrame(render);
+    }
